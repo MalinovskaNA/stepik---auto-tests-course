ed94799251a50f44b0c43d6fb67fd570155ebb5e
MalinovskayaNA <MalinovskayaNA@cbr.ru>
2020-04-20 20:21:50 +0300

    решение задачи из модуля 2 урок 3 шаг 6 - переход на новую вкладку

---
+++ lesson23_step6.py
@@ -0,0 +1,34 @@
+﻿from selenium import webdriver
+import time 
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/redirect_accept.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    
+    btn = browser.find_element_by_class_name("btn")
+    btn.click()
+
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+
+    inputY = browser.find_element_by_id("answer")
+    inputY.send_keys(y)
+
+    button = browser.find_element_by_class_name("btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
